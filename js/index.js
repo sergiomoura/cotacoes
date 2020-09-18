e3b99a262db65b2dcba44f20f99afb30897b84ab
Sérgio Moura <smouracalmon@gmail.com>
2020-09-18 10:41:13 -0300

    Exibindo cotaçao principal

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const cotacoes = {
         CAD:1.5567,HKD:9.1428,ISK:162.2,PHP:57.167,
         DKK:7.4396,HUF:361.16,CZK:26.747,AUD:1.6152,
         RON:4.86,SEK:10.408,IDR:17501.07,INR:86.861,
-        BRL:6.2117,RUB:88.6075,HRK:7.5418,JPY:123.38,
+        BRL:6.2999,RUB:88.6075,HRK:7.5418,JPY:123.38,
         THB:36.801,CHF:1.0742,SGD:1.6039,PLN:4.4579,
         BGN:1.9558,TRY:8.9037,CNY:7.9861,NOK:10.7238,
         NZD:1.7533,ZAR:19.2673,USD:1.1797,MXN:24.8461,
@@ -13,3 +13,18 @@ const cotacoes = {
     base:"EUR",
     date:"2020-09-17"
 }
+
+let moedaPrincipal = "BRL";
+
+const exibirCotacaoPrincipal = () => {
+    
+    // Escrevendo as siglas das moedas nos locais adequados
+    document.getElementById("moedaBase").innerHTML = cotacoes.base;
+    document.getElementById('moedaPrincipal').innerHTML = moedaPrincipal;
+
+    // Escrevendo o valor da cotação
+    document.getElementById("cotacaoPrincipal").innerHTML = cotacoes.rates[moedaPrincipal].toFixed(4);
+    
+}
+
+exibirCotacaoPrincipal();
